6aa6bd21b3cfccca616f4f4266be2240fcf092ce
listrikk <interessen76@mail.ru>
2025-04-02 16:39:00 +0700

    Update user.py

--- user.py
+++ user.py
@@ -1,31 +1,9 @@
-import battle
-import history
-import location
-import training
-import time
-import npc
-
-file = open('end.txt', 'r')
-if file.read() == '':
-    file.close
-    file = open('end.txt', 'a')
-    file.write('Твои концовки:')
-    file.close
-else:
-    file.close
-while True:
-    print('Вы готовы начать игру? Да/нет')
-    answer_game = input().lower()
-    if answer_game == 'да':
-        # history.video()
-        # time.sleep(3)
-        training.training()
-    elif answer_game == 'концовки':
-        file = open('end.txt', 'r')
-        data = file.read()
-        print(''.join(data))
-        file.close()
-    else:
-        print('Вы ответили нет или ввели не то. Пока.')
-        quit()
+class Robot:
+    def __init__(self, health, power, energy, point, beautiful,name):
+        self.health = health
+        self.power = power
+        self.energy = energy
+        self.point = point
+        self.beautiful = beautiful
+        self.name = name
 
